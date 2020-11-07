120f6ad1e9803f071f7818751ad1304e250a1055
kristokrugger <kristoelijahkrugger@gmail.com>
2020-11-06 17:39:27 -0800

    modified title animation for maze

--- Code/Reinforcement/maze.py
+++ Code/Reinforcement/maze.py
@@ -209,16 +209,12 @@ class maze:
         ax.set_ylim(0,self.height)
         ax.grid(True)
         title = ax.set_title("Q Learning Process")
-        # episode_label_posy, episode_label_posx = ax.get_ylim()[1], ax.get_xlim()[1]
-        # episode_label = ax.text(0.895*episode_label_posx, 1.02*episode_label_posy,
-                    # "", size=12, ha="center", animated=False)
         Q = ax.quiver(X, Y, Usave[0], Vsave[0], pivot='tail', color='r', units='inches')
         # frame update function
         def update_quiver(frame,Q,Usave,Vsave):
-            title = ax.set_title(f"Q Learning Process (Episode: {frame})")
-            #episode_label.set_text(f"Episode: {frame}")
+            title = ax.set_title(f"Q Learning Process -Episode: {frame}")
             Q.set_UVC(Usave[frame],Vsave[frame])
-            return Q,title
+            return Q, title
 
         # you need to set blit=False, or the first set of arrows never gets # cleared on subsequent frames
         ani = animation.FuncAnimation(fig, update_quiver, frames=nframe, fargs=(Q,Usave,Vsave),
